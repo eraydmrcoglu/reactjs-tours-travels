c2c9904ee2855d258a1c5f929dd137e652e7cd08
Eray Demircioglu <75967993+eraydmrcoglu@users.noreply.github.com>
2024-09-03 14:32:56 +0300

    Tour&Travel

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,1 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+export const BASE_URL = 'https://travel-booking-tvgv.onrender.com/api/v1'
